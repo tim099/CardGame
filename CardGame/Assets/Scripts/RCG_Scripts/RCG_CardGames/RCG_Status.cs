77885b1795fa02ebeefccdbb2ee5723a9021c04d
jackraken <apple894894@gmail.com>
2020-10-13 03:09:11 +0800

    update unit status

--- CardGame/Assets/Scripts/RCG_Scripts/RCG_CardGames/RCG_Status.cs
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_CardGames/RCG_Status.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace RCG{
     public class RCG_Status : MonoBehaviour
     {
         protected int m_amount = 0;
         protected int m_round = 0;
+
+        public TextMeshProUGUI m_counter_text;
         // Start is called before the first frame update
         void Start()
         {
@@ -17,6 +21,12 @@ namespace RCG{
         void Update()
         {
             
+        }
+
+        private void awake(){
+            if(!m_counter_text){
+                
+            }
         }
 
         public void StatusStart(){
