d7284c3a5d9576b77df40f6b6cdef654018e889c
tim1125 <tim11251994@gmail.com>
2020-10-31 19:26:32 +0800

    修正牌堆置頂功能

--- CardGame/Assets/Scripts/RCG_Scripts/RCG_UI_Scripts/RCG_ShowCardUI.cs
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_UI_Scripts/RCG_ShowCardUI.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UCL.TweenLib;
+using UnityEngine.UI;
+
 namespace RCG
 {
     public class RCG_ShowCardUI : MonoBehaviour
     {
         public RCG_CardRowsDisplayUI m_CardRowsDisplayUITmp;
         public Transform m_HidePos;
+        public ScrollRect m_ScrollRect;
         Vector3 m_ShowPos = Vector3.zero;
         List<RCG_CardRowsDisplayUI> m_CardRowsDisplayUIs = new List<RCG_CardRowsDisplayUI>();
         public void Init() {
@@ -16,7 +19,7 @@ namespace RCG
             m_ShowPos = transform.position;
         }
         public void Show(List<RCG_CardData> cards) {
-            gameObject.SetActive(true);
+            
             int card_num_in_row = m_CardRowsDisplayUITmp.m_CardDisplayer.Count;
             while(cards.Count > m_CardRowsDisplayUIs.Count * card_num_in_row) {
                 var new_row = Instantiate(m_CardRowsDisplayUITmp, m_CardRowsDisplayUITmp.transform.parent);
@@ -26,18 +29,25 @@ namespace RCG
             for(int i = 0; i < m_CardRowsDisplayUIs.Count; i++) {
                 var row = m_CardRowsDisplayUIs[i];
                 int count = i * card_num_in_row;
-                if(count <= cards.Count - card_num_in_row) {
+                if(count < cards.Count - card_num_in_row) {
                     row.Show(cards, count, card_num_in_row);
-                } else if(count <= cards.Count) {
+                } else if(count < cards.Count) {
                     row.Show(cards, count, cards.Count - count);
                 } else {
                     row.Hide();
                 }
             }
+            GetComponent<RectTransform>().ForceRebuildLayoutImmediate();
+
+            gameObject.SetActive(true);
             transform.position = m_HidePos.position;
-            transform.localScale = Vector3.zero;
+            transform.localScale = new Vector3(0.3f,0.3f,1f);
+            m_ScrollRect.ToTop();
             var twn = transform.UCL_Scale(0.3f, 1f).SetEase(EaseType.OutElastic);
             twn.AddComponent(transform.TC_Move(m_ShowPos));
+            //twn.OnComplete(() => {
+            //    m_ScrollRect.ToTop();
+            //});
             twn.Start();
 
         }
@@ -48,6 +58,7 @@ namespace RCG
             twn.AddComponent(transform.TC_Move(m_HidePos));
             twn.OnComplete(() => {
                 gameObject.SetActive(false);
+                transform.position = m_ShowPos;
             });
             twn.Start();
         }
