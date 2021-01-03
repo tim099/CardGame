5fc265ab94723554af8e29a52caf13c87baed133
tim1125 <tim11251994@gmail.com>
2021-01-03 21:01:05 +0800

    加入大地圖架構

---
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_CityButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace RCG
+{
+    public class RCG_CityButton : MonoBehaviour
+    {
+        public string m_CityName;
+        public Text m_CityNameText;
+        public Button m_CityButton;
+        RCG_BigMap p_Map;
+        virtual public void Init(RCG_BigMap _Map) {
+            p_Map = _Map;
+            m_CityNameText.text = m_CityName;
+            m_CityButton.onClick.AddListener(delegate () {
+                p_Map.SelectCity(m_CityName);
+            });
+        }
+    }
+}
