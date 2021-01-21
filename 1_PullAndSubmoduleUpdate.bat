195d1aaa4e2293ddc7973c281e9b40f6fa51f187
tim099 <tim11251994@gmail.com>
2021-01-21 17:44:00 +0800

    Update UCL Update 1_PullAndSubmoduleUpdate.bat

--- 1_PullAndSubmoduleUpdate.bat
+++ 1_PullAndSubmoduleUpdate.bat
@@ -1,4 +1,4 @@
-git pull
+git pull origin Working/GameFlowPrototype
 git submodule update --init --remote --recursive
 git submodule foreach -q --recursive "git checkout Dev;git pull origin Dev"
 cmd
