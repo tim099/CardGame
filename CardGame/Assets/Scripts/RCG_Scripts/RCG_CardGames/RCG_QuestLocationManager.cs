13c3417aeb65dd6fb1a5af7c650c0a57e0010ec7
jackraken <apple894894@gmail.com>
2021-01-24 01:14:57 +0800

    update big map layout

--- CardGame/Assets/Scripts/RCG_Scripts/RCG_CardGames/RCG_QuestLocationManager.cs
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_CardGames/RCG_QuestLocationManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RCG {
     public class RCG_QuestLocationManager : MonoBehaviour
@@ -19,11 +20,16 @@ namespace RCG {
         }
 
         public void showHoverPanel(Transform pos) {
-            m_quest_hover_panel.position = pos.position + Vector3.up * 5;
+            m_quest_hover_panel.SetParent(pos);
+            m_quest_hover_panel.position = pos.position + Vector3.up * 8;
             m_quest_hover_panel.gameObject.SetActive(true);
+            m_quest_hover_panel.GetComponentInChildren<Text>().text = pos.gameObject.GetComponentInChildren<RCG_CityButton>().m_CityNameText.text;
+            pos.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 150);
+            m_quest_hover_panel.GetComponent<Button>().onClick = pos.gameObject.GetComponentInChildren<Button>().onClick;
         }
 
-        public void hideHoverPanel() {
+        public void hideHoverPanel(Transform pos) {
+            pos.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(100, 100);
             m_quest_hover_panel.gameObject.SetActive(false);
         }
     }
