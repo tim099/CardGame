4ef2155bf440111b1f567be095491f47b2e83600
jackraken <apple894894@gmail.com>
2021-01-25 02:16:57 +0800

    Add character UI prototype

--- CardGame/Assets/Scripts/RCG_Scripts/RCG_GameService/RCG_DataService.cs
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_GameService/RCG_DataService.cs
@@ -8,12 +8,23 @@ namespace RCG
     {
 
     }
+
+    public class RCG_QuestData
+    {
+        public string m_location;
+        public string[] m_avilible_quests; 
+    }
+
     public class RCG_DataService : UCL.Core.Game.UCL_GameService
     {
         public RCG_DataService ins = null;
+        public List<RCG_CharacterData> m_all_character_data;
         public override void Init() {
             base.Init();
             ins = this;
+
+            m_all_character_data = new List<RCG_CharacterData>();
+            m_all_character_data.Add(new RCG_CharacterData("Tina", 20, 20, 1));
         }
     }
 }
---
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_UI_Scripts/RCG_MapCharacterUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RCG { 
+    public class RCG_MapCharacterUI : MonoBehaviour
+    {
+        private List<RCG_CharacterData> m_all_character_data;
+        public Transform m_character_panel;
+        private List<Transform> m_panels;
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_panels = new List<Transform>();
+            LoadCharacterData();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void ClearCharacterData()
+        {
+            foreach (var panel in m_panels)
+            {
+                Destroy(panel);
+            }
+            m_panels = new List<Transform>();
+        }
+
+        public void LoadCharacterData()
+        {
+            Debug.Log("LoadCharacterData");
+            ClearCharacterData();
+
+            m_all_character_data = RCG_GameManager.ins.GetDataService().m_all_character_data;
+            foreach (var character_data in m_all_character_data)
+            {
+                var new_panel = Instantiate(m_character_panel, transform);
+                m_panels.Add(new_panel);
+            }
+        }
+    }
+}
