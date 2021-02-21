b7f52984a2f1c80bbe387b18239f78c00a7fc8ba
Luke <apple894894@gmail.com>
2021-02-21 19:00:01 +0800

    update party UI

--- CardGame/Assets/Scripts/RCG_Scripts/RCG_UI_Scripts/RCG_CharacterUI.cs
+++ CardGame/Assets/Scripts/RCG_Scripts/RCG_UI_Scripts/RCG_CharacterUI.cs
@@ -7,6 +7,9 @@ namespace RCG
     public class RCG_CharacterUI : MonoBehaviour
     {
         public RCG_CharacterDetailPanel m_DetailPanel;
+        public RCG_CharacterPanel m_CharacterPanel;
+        bool m_Showed = true;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -16,11 +19,25 @@ namespace RCG
         // Update is called once per frame
         void Update()
         {
-
+            if (RCG_BattleManager.ins) { //can be better QWQ
+                if (m_Showed && RCG_BattleManager.ins.gameObject.activeInHierarchy)
+                {
+                    m_DetailPanel.gameObject.SetActive(false);
+                    hidePanels();
+                    m_Showed = false;
+                }
+                else if (!m_Showed && !RCG_BattleManager.ins.gameObject.activeInHierarchy)
+                {
+                    m_DetailPanel.gameObject.SetActive(false);
+                    showPanels();
+                    m_Showed = true;
+                }
+            }
         }
 
         void Awake()
         {
+            InitUI();
             m_DetailPanel.gameObject.SetActive(false);
         }
 
@@ -28,6 +45,30 @@ namespace RCG
         {
             m_DetailPanel.SetCharacterData(data);
             m_DetailPanel.Show();
+        }
+
+        public void InitUI()
+        {
+            foreach (var character_data in RCG_DataService.ins.m_CharacterDatas)
+            {
+                var panel = Instantiate(m_CharacterPanel, transform);
+                panel.GetComponent<RCG_CharacterPanel>().Init(character_data);
+            }
+        }
+
+        public void hidePanels()
+        {
+            foreach (Transform child in transform) { 
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        public void showPanels()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
         }
     }
 }
